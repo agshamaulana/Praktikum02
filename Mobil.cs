d3ac4a67c1f72a71761ed5b95abc46ca9cb6d367
agshamaulana <agshamaulana14@students.amikom.ac.id>
2023-03-28 00:03:50 -0800

    first commit

---
+++ Mobil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktikum02
+{
+    internal class Mobil
+    {
+        public string warna;
+        public int JumlahPintu;
+        public string merk;
+        public string model;
+        public int TahunKeluar;
+
+        public void Gas(string Kecepatan)
+        {
+            Console.WriteLine("Mobil {0} berjalan dengan kecepatan {1}", model, Kecepatan);
+        }
+        public void Klakson(string suara)
+        {
+            Console.WriteLine("Dengan Suara Klakson {0}", suara);
+        }
+        public void TampilkanInfo()
+        {
+            Console.WriteLine("Mobil saya berwarna {0}, merek {1}, model {2}, keluaran tahun {3}, dengan jumlah pintu sebanyak {4}", warna, merk, model, TahunKeluar, JumlahPintu);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktikum02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mobil mobil1 = new Mobil();
+            mobil1.warna = "Hijau";
+            mobil1.JumlahPintu = 4;
+            mobil1.merk = "Merkedel";
+            mobil1.model = "Mercedel Benz Aventini";
+            mobil1.TahunKeluar = 2050;
+
+            mobil1.Gas("200 km/h");
+            mobil1.Klakson("Tititititit");
+            mobil1.TampilkanInfo();
+
+            Console.WriteLine("\n");
+            Console.WriteLine("=======================================");
+
+            Console.ReadLine();
+        }
+    }
+}
